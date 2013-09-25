887f6524e8a23d693ba4ec9f998f49c8d438042c
Kelmer Perez <kelmerp@gmail.com>
2013-09-25 16:48:27 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HeadlineAnalysis::Application.config.secret_key_base = 'b6bd9704200fd433304dbc1eb129a669df4b252a06d3abdf0773d2cf2b8a7a329642868fd68ab3c9a75af096bd4a084323efed998920dd53fa520d2c2850f355'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HeadlineAnalysis::Application.config.session_store :cookie_store, key: '_headline_analysis_session'
