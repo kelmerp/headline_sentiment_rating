055d19712a608503a737e649bc110641481e5782
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-26 13:45:02 -0500

    Fixed indentation

--- spec/models/source_spec.rb
+++ spec/models/source_spec.rb
@@ -5,7 +5,8 @@ describe Source do
   it "should have many headlines" do
     should have_many(:headlines)
   end
-    it "validates the presence of a name" do
+
+  it "validates the presence of a name" do
     should validate_presence_of(:name)
   end
 end
