b3bdfc2bf90a2adf2fbc47c9e5968bff9171e693
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-26 17:20:47 -0500

    modified alchemy output so that all sentiment enigines have consistent output

--- app/api/alchemy.rb
+++ app/api/alchemy.rb
@@ -1,10 +1,12 @@
 class Alchemy
-
   def self.alchemy_helper
     AlchemyAPI.key = ENV['alchemy']
   end
 
   def self.get_sentiment(string)
-    AlchemyAPI.search(:sentiment_analysis, :text => string)
+    raw_results = AlchemyAPI.search(:sentiment_analysis, :text => string)
+    raw_score = 0.0 if raw_results["score"] == nil
+    { "sentiment_description" => raw_results["type"],
+      "sentiment_score" => raw_score }
   end
 end
