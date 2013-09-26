084769166657e988fe5706a730ef81c7adbcdb60
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-26 13:40:28 -0500

    Changed validation of content length to 255

--- app/models/headline.rb
+++ app/models/headline.rb
@@ -1,6 +1,6 @@
 class Headline < ActiveRecord::Base
   belongs_to :source
-  validates :content, length: { maximum: 250}
+  validates :content, length: { maximum: 255}
   validates :date, presence: true
   validates :archive_url, presence: true
 end
