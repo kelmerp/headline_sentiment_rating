f0ede36af748bdf28492fbce4a873cb4f276c1fd
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-26 18:07:45 -0500

    added validations for sentiment engine field on Headline model

--- app/models/headline.rb
+++ app/models/headline.rb
@@ -3,4 +3,6 @@ class Headline < ActiveRecord::Base
   validates :content, length: { maximum: 255}
   validates :date, presence: true
   validates :archive_url, presence: true
+  validates :sentiment_engine, :inclusion => { :in => %w(Alchemy ViralHeat
+                                                  Sentimental Sentimentalizer)}
 end
