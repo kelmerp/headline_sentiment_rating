f0880cd2b4fb39f60d641d68a0673316428e2753
Kelmer Perez <kelmerp@gmail.com>
2013-09-26 20:56:26 -0500

    added seed.rb to pull in seed data from csv file

--- app/api/alchemy.rb
+++ app/api/alchemy.rb
@@ -5,8 +5,8 @@ class Alchemy
 
   def self.get_sentiment(string)
     raw_results = AlchemyAPI.search(:sentiment_analysis, :text => string)
-    raw_score = 0.0 if raw_results["score"] == nil
+    raw_results["score"] = 0.0 if raw_results["score"] == nil
     { "sentiment_description" => raw_results["type"],
-      "sentiment_score" => raw_score }
+      "sentiment_score" => raw_results["score"] }
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'csv'
+path = './db/seeds/headline.csv'
+
+# ["http://www.cnn.com", "http://web.archive.org/web/20120927230206/http://www.cnn.com/","20120927230206", "Teen dies playing 'pass out' game"]
+
+CSV.foreach(path) do |row|
+  s = Source.find_or_create_by(:name => row[0])
+  h = Headline.new(:date => row[2], :archive_url => row[1], :content => row[3])
+  h.source_id = s.id
+  sentiment_data = Alchemy.get_sentiment(h.content)
+  h.sentiment_description = sentiment_data['sentiment_description']
+  h.sentiment_score = sentiment_data['sentiment_score']
+  h.sentiment_engine = "Alchemy"
+  h.save
+end
