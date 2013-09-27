d01bc1e234adcc9bce60ab64db8f2605a38dada1
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-27 15:18:23 -0500

    added a self.date and self.get_average method

--- app/models/headline.rb
+++ app/models/headline.rb
@@ -5,4 +5,13 @@ class Headline < ActiveRecord::Base
   validates :archive_url, presence: true
   validates :sentiment_engine, :inclusion => { :in => %w(Alchemy ViralHeat
                                                   Sentimental Sentimentalizer)}
+  def self.date(some_date)
+    search_date = DateTime.parse(some_date.to_s)
+    date_range = search_date.beginning_of_day...search_date.end_of_day
+    self.where(date: date_range)
+  end
+
+  def self.get_average(some_date)
+    self.date(some_date).average(:sentiment_score)
+  end
 end
