9519088a5c56bedd07156f2198a7c9ca98e1f81b
Kelmer Perez <kelmerp@gmail.com>
2013-09-27 09:51:28 -0500

    added seeding from healine.csv file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,19 @@
 require 'csv'
 path = './db/seeds/headline.csv'
 
-# ["http://www.cnn.com", "http://web.archive.org/web/20120927230206/http://www.cnn.com/","20120927230206", "Teen dies playing 'pass out' game"]
-
 CSV.foreach(path) do |row|
-  s = Source.find_or_create_by(:name => row[0])
-  h = Headline.new(:date => row[2], :archive_url => row[1], :content => row[3])
+  s = Source.find_or_create_by(:name => row[0].to_s)
+  h_date = row[2].strip
+  h_url = row[1].strip
+  h_content = row[3].strip
+  next if (h_date == nil || h_url == nil || h_content == nil)
+  h = Headline.new(:date => h_date, :archive_url => h_url, :content => h_content)
   h.source_id = s.id
-  sentiment_data = Alchemy.get_sentiment(h.content)
+  begin
+    sentiment_data = Alchemy.get_sentiment(h.content) if h.content
+  rescue
+    next
+  end
   h.sentiment_description = sentiment_data['sentiment_description']
   h.sentiment_score = sentiment_data['sentiment_score']
   h.sentiment_engine = "Alchemy"
