f1d06eba1e86ec06eab2a7c01756b8f7c504f674
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-29 13:44:57 -0500

    Added new migration and model called SentimentData and changed old models to reflect new models

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130929145727) do
+ActiveRecord::Schema.define(version: 20130929180737) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,19 @@ ActiveRecord::Schema.define(version: 20130929145727) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "sentiment_data", force: true do |t|
+    t.decimal  "sentiment_score"
+    t.string   "sentiment_description"
+    t.string   "sentiment_engine"
+    t.integer  "headlines_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sentiment_data", ["sentiment_description"], name: "index_sentiment_data_on_sentiment_description", using: :btree
+  add_index "sentiment_data", ["sentiment_engine"], name: "index_sentiment_data_on_sentiment_engine", using: :btree
+  add_index "sentiment_data", ["sentiment_score"], name: "index_sentiment_data_on_sentiment_score", using: :btree
 
   create_table "sources", force: true do |t|
     t.string   "name"
