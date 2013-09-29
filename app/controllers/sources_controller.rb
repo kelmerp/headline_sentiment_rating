0ac854c5ad6c2c63b8986900631f9b594cd9fa80
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-29 17:39:56 -0500

    dots on graph

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -8,20 +8,37 @@ class SourcesController < ApplicationController
   end
 
   def cnn
-    source = Source.find_by(name: "http://www.cnn.com")
+
     dates = []
-    final_array = []
-    source.headlines.limit(100).each do |headline|
-      dates << headline.date
-    end
-    dates.uniq!.reverse
-    dates.each do |date|
-      score = source.headlines.get_average(date.to_s)
-      object = {cnn: score.to_f, date: date}
-      final_array << object
+    json_object = []
+
+    source = Source.find_by(name: 'http://www.cnn.com')
+
+    source.headlines.limit(5000).each {|d| dates << d.date.strftime("%Y%m%d")}
+    dates.uniq!.sort.reverse.each do |date|
+      object = {date: DateTime.parse(date).to_i, score: source.headlines.get_average(date).to_f }
+      json_object << object
     end
 
-    render json: final_array[0..50].to_json
+    render json: json_object.to_json
+
+  end
+
+  def fox
+
+    dates = []
+    json_object = []
+
+    source = Source.find_by(name: 'http://www.foxnews.com')
+
+    source.headlines.limit(5000).each {|d| dates << d.date.strftime("%Y%m%d")}
+    dates.uniq!.sort.reverse.each do |date|
+      object = {date: DateTime.parse(date).to_i, score: source.headlines.get_average(date).to_f }
+      json_object << object
+    end
+
+    render json: json_object.to_json
+    
   end
 
   # def fox
