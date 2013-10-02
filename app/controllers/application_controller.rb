dddf76e72c0e65b59b086e6509fbc4dbf8d31193
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-01 20:14:36 -0500

    switched out some slightly faster methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,9 @@ class ApplicationController < ActionController::Base
     json_object = []
 
     source = Source.find_by(name: source)
-    dates = source.headlines.pluck(:date).uniq!
+    dates = source.headlines.order("date DESC").pluck(:date).uniq
 
-    dates.sort.reverse.each do |date|
+    dates.each do |date|
       object = {date: DateTime.parse(date.to_s).to_i, score: source.get_average_for_date_cached(date).to_f, source: source.name }
       json_object << object
     end
